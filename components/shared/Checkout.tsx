6c8923029d59ae13c31ced1449c1a2a587d6445e
Alfian <muhammadalfiannovanto17@gmail.com>
2025-05-14 17:29:27 +0700

    update payment midtrans1.9

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -4,9 +4,11 @@ import React, { useEffect, useTransition } from "react";
 import { IEvent } from "@/lib/database/models/event.model";
 import { Button } from "../ui/button";
 import { checkoutOrder } from "@/lib/actions/order.actions";
+import { useUser } from "@clerk/nextjs"; // 🔥 import user Clerk
 
 const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
   const [isPending, startTransition] = useTransition();
+  const { user: clerkUser } = useUser(); // 🧠 Ambil user Clerk
 
   useEffect(() => {
     const query = new URLSearchParams(window.location.search);
@@ -25,9 +27,10 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
     const order = {
       eventTitle: event.title,
       eventId: event._id,
-      price: event.price,
+      price: Number(event.price),
       isFree: event.isFree,
       buyerId: userId,
+      buyerEmail: clerkUser?.emailAddresses[0]?.emailAddress || "", // ✅ FIXED
     };
 
     try {
